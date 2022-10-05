c3ed4d0f0f4124e38b1b4df067f92e3db79df74c
Delfi1 <fezdx@mail.ru>
2022-10-05 19:03:35 +0300

    Добавьте файлы проекта.

---
+++ Menu.Designer.cs
@@ -0,0 +1,71 @@
+﻿namespace DelfiApp
+{
+    partial class Menu
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.kryptonPalette1 = new ComponentFactory.Krypton.Toolkit.KryptonPalette(this.components);
+            this.SuspendLayout();
+            // 
+            // kryptonPalette1
+            // 
+            this.kryptonPalette1.FormStyles.FormMain.StateCommon.Back.Color1 = System.Drawing.Color.FromArgb(((int)(((byte)(250)))), ((int)(((byte)(252)))), ((int)(((byte)(252)))));
+            this.kryptonPalette1.FormStyles.FormMain.StateCommon.Back.Color2 = System.Drawing.Color.FromArgb(((int)(((byte)(250)))), ((int)(((byte)(252)))), ((int)(((byte)(252)))));
+            this.kryptonPalette1.FormStyles.FormMain.StateCommon.Border.DrawBorders = ((ComponentFactory.Krypton.Toolkit.PaletteDrawBorders)((((ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Top | ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Bottom) 
+            | ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Left) 
+            | ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Right)));
+            this.kryptonPalette1.FormStyles.FormMain.StateCommon.Border.GraphicsHint = ComponentFactory.Krypton.Toolkit.PaletteGraphicsHint.None;
+            this.kryptonPalette1.FormStyles.FormMain.StateCommon.Border.Rounding = 6;
+            this.kryptonPalette1.HeaderStyles.HeaderForm.StateCommon.Back.Color1 = System.Drawing.Color.Aquamarine;
+            this.kryptonPalette1.HeaderStyles.HeaderForm.StateCommon.Back.Color2 = System.Drawing.Color.Azure;
+            this.kryptonPalette1.HeaderStyles.HeaderForm.StateCommon.ButtonEdgeInset = 10;
+            this.kryptonPalette1.HeaderStyles.HeaderForm.StateCommon.Content.Padding = new System.Windows.Forms.Padding(10, -1, -1, -1);
+            // 
+            // Menu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(250)))), ((int)(((byte)(252)))), ((int)(((byte)(252)))));
+            this.ClientSize = new System.Drawing.Size(832, 453);
+            this.Name = "Menu";
+            this.Palette = this.kryptonPalette1;
+            this.PaletteMode = ComponentFactory.Krypton.Toolkit.PaletteMode.Custom;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "De:Menu";
+            this.TextExtra = "Delfi:Menu";
+            this.Load += new System.EventHandler(this.Menu_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private ComponentFactory.Krypton.Toolkit.KryptonPalette kryptonPalette1;
+    }
+}
+
---
+++ Menu.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.IO;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ComponentFactory.Krypton.Toolkit;
+
+namespace DelfiApp
+{
+    public partial class Menu : KryptonForm
+    {
+        string ver = "0.2913";
+        WebClient client = new WebClient();
+        string fullPath = Application.StartupPath.ToString();
+
+        void setup_update(bool in_st)
+        {
+            if (client.DownloadString("https://raw.githubusercontent.com/Delfi1/DeTest/master/Test1/version.txt").Contains(ver))
+            {
+                if (!in_st)
+                {
+                    MessageBox.Show("Версия приложения:" + ver + "\n" + "Версия приложения на сервере: " + client.DownloadString("https://raw.githubusercontent.com/Delfi1/DeTest/master/Test1/version.txt"), "Уведомление", MessageBoxButtons.OK);
+                }
+            }
+            else
+            {
+                if (!in_st) { MessageBox.Show("Версия приложения:" + ver + "\n" + "Версия приложения на сервере: " + client.DownloadString("https://raw.githubusercontent.com/Delfi1/DeTest/master/Test1/version.txt"), "Уведомление", MessageBoxButtons.OK); }
+                MessageBox.Show("Обнаружена новая версия! Идет установка файлов...", "Update", MessageBoxButtons.OK);
+                File.Move(fullPath + "\\DeWorld.exe", fullPath + "\\DeWorld_old.exe");
+                string requestString = @"https://github.com/Delfi1/DeTest/blob/master/Test1/bin/Release/Test1.exe?raw=true";
+                HttpClient httpClient = new HttpClient();
+                var GetTask = httpClient.GetAsync(requestString);
+                GetTask.Wait(1000); // WebCommsTimeout is in milliseconds
+
+                if (!GetTask.Result.IsSuccessStatusCode)
+                {
+                    // write an error
+                    return;
+                }
+
+                using (var fs = new FileStream(fullPath + "\\DeWorld.exe", FileMode.CreateNew))
+                {
+                    var ResponseTask = GetTask.Result.Content.CopyToAsync(fs);
+                    ResponseTask.Wait(1000);
+                }
+                System.Diagnostics.Process.Start(fullPath + "\\DeWorld.exe");
+                Application.Exit();
+            }
+        }
+        async void TextExtraEdit()
+        {
+            await Task.Delay(300);
+            string DeWorld = "Delfi:World";
+            int step = 0;
+            string PrintVer = "Version: " + ver;
+            string Check = "Checking for a new version...";
+            while (true){
+                while (step <= DeWorld.Length)
+                {
+                    TextExtra = DeWorld.Substring(0, step++);
+
+                    await Task.Delay(175);
+                }
+                await Task.Delay(2000);
+                step = 0;
+                while (step <= PrintVer.Length)
+                {
+                    TextExtra = PrintVer.Substring(0, step++);
+
+                    await Task.Delay(175);
+                }
+                await Task.Delay(2000);
+                step = 0;
+                while (step <= Check.Length)
+                {
+                    TextExtra = Check.Substring(0, step++);
+
+                    await Task.Delay(175);
+                }
+                setup_update(true);
+                step = 0;
+                await Task.Delay(2000);
+            }
+        }
+        public Menu()
+        {
+            InitializeComponent();
+        }
+
+        private void Menu_Load(object sender, EventArgs e)
+        {
+            TextExtraEdit();
+        }
+    }
+}
