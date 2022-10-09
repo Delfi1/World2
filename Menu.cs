e388099ffa6bae78b549d387929e2a404aa303c3
Delfi1 <fezdx@mail.ru>
2022-10-09 15:27:10 +0300

    World_0_0_6

--- Menu.cs
+++ Menu.cs
@@ -18,7 +18,7 @@ namespace DelfiApp
     {
         int get_hg1;
         int get_wd1;
-        string ver = "0.2926";
+        string ver = "0.2928";
         WebClient client = new WebClient();
         string fullPath = Application.StartupPath.ToString();
         DeLog Log = new DeLog();
@@ -314,6 +314,7 @@ namespace DelfiApp
                     }
                     StreamWriter sw = new StreamWriter(fullPath + "\\World\\World_ver.txt");
                     sw.WriteLine(client.DownloadString("https://raw.githubusercontent.com/Delfi1/DeWorld/master/World_version.txt"));
+                    sw.Close();
                 }
                 else
                 {
